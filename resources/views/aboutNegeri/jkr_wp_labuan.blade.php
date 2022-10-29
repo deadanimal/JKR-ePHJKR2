0a3cc328c52d054d52d5edb0ef7655789050bac1
hayeh <haris.zahari@pipeline-network.com>
2022-10-29 11:21:42 +0800

    borang pendaftaran projek dan about detail senarai wilayah

---
+++ resources/views/aboutNegeri/jkr_wp_labuan.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.anon')
+
+@section('content')
+
+<div class="col">
+    <div class="row">
+        <div class="col-6 mt-5">
+            <img src="/assets/img/picture1.png" alt="">
+        </div>
+        <div class="col-6 mt-5">
+            <div class="col mt-3">
+                <h4>JKR Wilayah Labuan</h4>
+            </div>
+            <div class="col mt-4">
+                <h6 class="text-bold">
+                    Pengarah
+                </h6>
+                <p>Dato'Ir Ts. Hafizah Binti Zakaria</p>
+                
+            </div>
+
+            <div class="col mt-2">
+                <h6 class="text-bold">
+                    Setiausaha Pejabat
+                </h6>
+                <p>Bawani A/P Gunasekaran</p>
+                
+            </div>
+
+            <div class="col mt-2">
+                <h6 class="text-bold">
+                    Alamat Pejabat
+                </h6>
+                <p>Pejabat Timbalan Ketua Pengarah Kerja Raya (Sektor Pakar) <br>Tingkat 33, Menara Kerja Raya (Blok G) <br>Ibu Pejabat JKR, Jalan Sultan Salahuddin, <br>50480 Kuala Lumpur</p>
+                <p><i class="fas fa-blender-phone"></i>03-26188433 <br><i class="fas fa-blender-phone"></i>03-26188433</p>
+            </div>
+        </div>
+    </div>
+</div>
+    
+@endsection
