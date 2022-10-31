5876c1facd8d226d5352eb82afde0d1f1eb84b4b
hayeh <haris.zahari@pipeline-network.com>
2022-10-31 21:50:33 +0800

    hubungi kami

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -2,6 +2,20 @@
 
 @section('content')
 
-testing push to branch
+<div class="col mt-3">
+    <div class="card">
+        <div class="card-body">
+            <h2>HUBUNGI KAMI</h2>
+            <div class="col">
+                <p>
+                    Cawangan Alam Sekitar & Kecekapan Tenaga
+                </p><br>
+                <p class="mt-2"><i class="fas fa-blender-phone"></i>03-40518420 <br><i class="fas fa-blender-phone"></i>03-40518784 <br><i class="fas fa-mail-bulk"></i>penarafanhijau@jkr.gov.my</p>
+
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
 
 @endsection
