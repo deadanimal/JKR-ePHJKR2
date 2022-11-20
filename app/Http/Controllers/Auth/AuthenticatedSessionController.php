51bbcb78d3797b2bbeb6451e2089d62846d645cf
Afeez Aziz <afeezaziz@gmail.com>
2022-11-20 16:50:00 +0800

    Update AuthenticatedSessionController.php

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -35,9 +35,7 @@ class AuthenticatedSessionController extends Controller
             dd('user not sah');
         }
 
-        dd('1');
         $request->authenticate();
-
         $request->session()->regenerate();
 
         return redirect()->intended(RouteServiceProvider::HOME);
